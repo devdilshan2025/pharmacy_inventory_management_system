dc5c0892873a2f7884c3358b508233e3bde8bd47
devdilshan2025 <dsbandara4@gmail.com>
2025-11-25 06:59:58 +0530

    Add order login controller clss and order login UI

---
+++ src/main/java/Controller/OrderformController.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class OrderformController {
+
+    @FXML
+    private Button btnOrder;
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+
+
+    }
+
+}
--- src/main/java/Controller/PharmacyLoginformController.java
+++ src/main/java/Controller/PharmacyLoginformController.java
@@ -1,4 +1,44 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class PharmacyLoginformController {
+
+    @FXML
+    private Button btnAdmin;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private ImageView image1;
+
+    @FXML
+    void btnAdminOnAction(ActionEvent event) {
+
+    }
+
+
+    Stage stage = new Stage();
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/Order_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+
+    }
+
 }
