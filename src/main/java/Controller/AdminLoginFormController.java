11c6f1cae68bf39c3162a7c09ee86f11fcd0e900
devdilshan2025 <dsbandara4@gmail.com>
2025-11-26 07:17:06 +0530

    Add stock and supply login form and controller class

--- src/main/java/Controller/AdminLoginFormController.java
+++ src/main/java/Controller/AdminLoginFormController.java
@@ -2,10 +2,18 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminLoginFormController {
+
+    Stage  stage = new Stage();
 
     @FXML
     private Button btnLogin;
@@ -19,6 +27,24 @@ public class AdminLoginFormController {
     @FXML
     void btnLoginOnAction(ActionEvent event) {
 
+        String name = txtUserName.getText();
+        String password = txtPassword.getText();
+
+        if (name.equals("suwani") && password.equals("1234")) {
+
+
+            try {
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/Stock&SupplyLogin_form.fxml"))));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            stage.show();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Login Failed");
+            alert.setContentText("Invalied Username or Password");
+            alert.show();
+        }
     }
 
 }
