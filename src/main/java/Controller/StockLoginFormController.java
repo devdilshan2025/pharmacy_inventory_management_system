4ba0f28d0a0322060717d1cb782cb66e64a7c09c
devdilshan2025 <dsbandara4@gmail.com>
2025-11-27 14:21:56 +0530

    The connection to the Stock entity has been cteated

--- src/main/java/Controller/StockLoginFormController.java
+++ src/main/java/Controller/StockLoginFormController.java
@@ -1,13 +1,25 @@
 package Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.dto.StockInfo;
 
-public class StockLoginFormController {
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+import java.time.LocalDate;
+
+public class StockLoginFormController implements Initializable {
+
+    ObservableList<StockInfo> stockInfos = FXCollections.observableArrayList();
 
     @FXML
     private Button btnAdd;
@@ -25,6 +37,9 @@ public class StockLoginFormController {
     private TableColumn<?, ?> colExp;
 
     @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
     private TableColumn<?, ?> colName;
 
     @FXML
@@ -34,10 +49,13 @@ public class StockLoginFormController {
     private TableColumn<?, ?> colQuantity;
 
     @FXML
-    private TableView<?> tblStock;
+    private TableView<StockInfo> tblStock;
 
     @FXML
     private TextField txtExp;
+
+    @FXML
+    private TextField txtItemId;
 
     @FXML
     private TextField txtPrice;
@@ -66,4 +84,39 @@ public class StockLoginFormController {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Pharmacy", "root", "1234");
+            String sql = "SELECT * FROM item";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                StockInfo stockInfo =new StockInfo(
+                        resultSet.getString("ItemCode"),
+                        resultSet.getString("Name"),
+                        resultSet.getString("Brand"),
+                        resultSet.getDate("ExpeDate"),
+                        resultSet.getInt("Quantity"),
+                        resultSet.getDouble("UnitPrice")
+                );
+
+                stockInfos.add(stockInfo);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        colId.setCellValueFactory(new PropertyValueFactory<>("itemId"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colBrand.setCellValueFactory(new PropertyValueFactory<>("brand"));
+        colExp.setCellValueFactory(new PropertyValueFactory<>("expDate"));
+        colQuantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        tblStock.setItems(stockInfos);
+    }
 }
---
+++ src/main/java/model/dto/StockInfo.java
@@ -0,0 +1,21 @@
+package model.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class StockInfo {
+
+    private String itemId;
+    private String name;
+    private  String brand;
+    private Date expDate;
+    private int quantity;
+    private double price;
+}
