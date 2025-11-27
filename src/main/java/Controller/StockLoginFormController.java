edf621288c511b077083e6d533086f991f9f074f
devdilshan2025 <dsbandara4@gmail.com>
2025-11-27 08:25:04 +0530

    Add Stock login  form controller ada UI

---
+++ src/main/java/Controller/StockLoginFormController.java
@@ -0,0 +1,69 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StockLoginFormController {
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colBrand;
+
+    @FXML
+    private TableColumn<?, ?> colExp;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TableView<?> tblStock;
+
+    @FXML
+    private TextField txtExp;
+
+    @FXML
+    private TextField txtPrice;
+
+    @FXML
+    private TextField txtQuntity;
+
+    @FXML
+    private TextField txtbrand;
+
+    @FXML
+    private TextField txtname;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/Controller/StockSupplyLoginFormController.java
+++ src/main/java/Controller/StockSupplyLoginFormController.java
@@ -1,4 +1,39 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class StockSupplyLoginFormController {
+
+    Stage stage = new Stage();
+
+    @FXML
+    private Button btnStock;
+
+    @FXML
+    private Button btnSupply;
+
+    @FXML
+    void btnStockOnAction(ActionEvent event) {
+
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/StockLogin_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+
+    }
+
+    @FXML
+    void btnSupplyOnAction(ActionEvent event) {
+
+    }
+
 }
